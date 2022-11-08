4010245b338a1ed374459f9af2320695e1024909
Loc Dang <ext-loc.dang@spdigital.sg>
2022-11-08 13:54:15 +0700

    Draw map

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,34 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public bool controlEnabled = true;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+
     Vector2 move;
 
     SpriteRenderer spriteRenderer;
+
+    private Animator anim;
+    private enum MovementState { idle, running, jumping, falling };
 
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (controlEnabled) {
-            move.x = Input.GetAxis("Horizontal");
-        }
-        else {
-            move.x = 0;
-        }
-        ComputeVelocity();
+        move.x = Input.GetAxis("Horizontal");
+        UpdateAnimatorState();
     }
 
-    private void ComputeVelocity(){
-        if (move.x > 0.01f){
-            spriteRenderer.flipX = false;
-        } else if (move.x < -0.01f) {
+    private void UpdateAnimatorState() {
+        MovementState state;
+
+        if (move.x < 0f) {
+            state = MovementState.running;
             spriteRenderer.flipX = true;
+        } else if (move.x > 0f) {
+            state = MovementState.running;
+            spriteRenderer.flipX = false;
+        } else {
+            state = MovementState.idle;
         }
+
+        anim.SetInteger("state", (int)state);
     }
+
 }
